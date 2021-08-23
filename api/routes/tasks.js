69ba6f68e65f31da531022dff8f1ea97c1c27578
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-08-23 09:34:14 +0200

    GET single task, PATCH task, DELETE task routes and services added and tested

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAllTasks, createTask } = require("../../services/tasks");
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask,
+} = require("../../services/tasks");
 
-/* router.route("/").get(getAllTasks); */
 router.get("/", async (req, res) => {
   const tasks = await getAllTasks();
   res.status(200).json({ tasks });
 });
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const task = await getTask(id);
+  res.status(200).json({ task });
+});
 router.post("/", async (req, res) => {
   const task = await createTask(req.body);
   res.status(201).json({ task });
+});
+router.patch("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, isCompleted } = req.body;
+  const task = await updateTask(id, name, isCompleted);
+  res.status(200).json({ task });
+});
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const task = await deleteTask(id);
+  res.status(200).json({ task });
 });
 
 module.exports = router;
--- services/tasks.js
+++ services/tasks.js
@@ -2,6 +2,20 @@ const Task = require("../models/Task");
 
 const getAllTasks = async () => await Task.find({});
 
+const getTask = async (id) => await Task.find({ _id: id });
+
 const createTask = async (task) => await Task.create(task);
 
-module.exports = { getAllTasks, createTask };
+const deleteTask = async (id) => await Task.deleteOne({ _id: id });
+
+const updateTask = async (id, name, isCompleted) =>
+  await Task.findOneAndUpdate(
+    { _id: id },
+    { name, isCompleted },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask };
